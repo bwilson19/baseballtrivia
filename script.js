b0a12dfaa66159c5e23ec13f40584b69c5f00dea
bwilson19 <brendan.wilson19@gmail.com>
2020-01-08 12:38:12 -0800

    code refactor, using oop to create questions and randomize answers

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 // Global Variables
-const losers = document.querySelectorAll('.loser');
-const winner = document.querySelectorAll('.winner');
+
+const questionField = document.querySelector('.question');
+const correctField = document.querySelector('.correct');
+const wrongField1 = document.querySelector('.wrong1');
+const wrongField2 = document.querySelector('.wrong2');
+const wrongField3 = document.querySelector('.wrong3');
 const runs = document.querySelector('#run-count');
 const outs = document.querySelector('#out-count');
 const hits = document.querySelector('#hit-count');
@@ -26,14 +30,198 @@ const restartButton = document.querySelector('#restart-button');
 const directionsButton = document.querySelector('#directions-button');
 const directionsMenu = document.querySelector('#directions-menu');
 const closeWindowButton = document.querySelector('#close-window');
+const multiple = document.querySelector('.multiple-choice');
+
+// Placeholders
 
 let currentRuns = 0;
 let currentOuts = 0;
 let currentRunners = 0;
 let currentHits = 0;
-let zIndex = 0;
-let questionNumber = 1;
 let fieldNumber = 0;
+
+// Question objects
+
+class Question {
+  constructor(question, correct, wrong1, wrong2, wrong3) {
+    this.question = question;
+    this.correct = correct;
+    this.wrong1 = wrong1;
+    this.wrong2 = wrong2;
+    this.wrong3 = wrong3;
+  }
+}
+
+let question1 = new Question(
+  'Who was the NL MVP?',
+  '<img src="images/bellinger.jpg" alt="Cody Bellinger" />Cody Bellinger',
+  '<img src="images/arenado.jpg" alt="Nolan Arenado" />Nolan Arenado',
+  '<img src="images/bryant.jpg" alt="Kris Bryant" />Kris Bryant',
+  '<img src="images/yelich.jpg" alt="Christian Yelich" />Christian Yelich'
+);
+
+let question2 = new Question(
+  'Who was the AL MVP?',
+  '<img src="images/trout.jpg" alt="Mike Trout" />Mike Trout',
+  '<img src="images/judge.jpg" alt="Aaron Judge" />Aaron Judge',
+  '<img src="images/lindor.jpg" alt="Francisco Lindor" />Francisco Lindor',
+  '<img src="images/springer.jpg" alt="George Springer" />George Springer'
+);
+
+let question3 = new Question(
+  'Who won the World Series?',
+  '<img src = "images/nationals.png" alt = "Washington Nationals" /> Washington Nationals',
+  '<img src="images/yankees.png" alt="New York Yankees" />New York Yankees',
+  '<img src="images/astros.png" alt="Houston Astros" />Houston Astros',
+  '<img src="images/giants.png" alt="San Francisco Giants" />San Francisco Giants'
+);
+
+let question4 = new Question(
+  'Who was the AL Cy Young?',
+  'Mike Trout',
+  'Jacob DeGrom',
+  'Nolan Arenado',
+  'Christian Yelich'
+);
+
+let question5 = new Question(
+  'Who was the NL Cy Young?',
+  'Mike Trout',
+  'Jacob DeGrom',
+  'Nolan Arenado',
+  'Christian Yelich'
+);
+
+let question6 = new Question(
+  'Who was the NL Cy Young?',
+  'Mike Trout',
+  'Jacob DeGrom',
+  'Nolan Arenado',
+  'Christian Yelich'
+);
+
+let question7 = new Question(
+  'Who was the NL Cy Young?',
+  'Mike Trout',
+  'Jacob DeGrom',
+  'Nolan Arenado',
+  'Christian Yelich'
+);
+
+let question8 = new Question(
+  'Who was the NL Cy Young?',
+  'Mike Trout',
+  'Jacob DeGrom',
+  'Nolan Arenado',
+  'Christian Yelich'
+);
+
+let question9 = new Question(
+  'Who was the NL Cy Young?',
+  'Mike Trout',
+  'Jacob DeGrom',
+  'Nolan Arenado',
+  'Christian Yelich'
+);
+
+let question10 = new Question(
+  'Who was the NL Cy Young?',
+  'Mike Trout',
+  'Jacob DeGrom',
+  'Nolan Arenado',
+  'Christian Yelich'
+);
+
+let question11 = new Question(
+  'Who was the NL Cy Young?',
+  'Mike Trout',
+  'Jacob DeGrom',
+  'Nolan Arenado',
+  'Christian Yelich'
+);
+
+let question12 = new Question(
+  'Who was the NL Cy Young?',
+  'Mike Trout',
+  'Jacob DeGrom',
+  'Nolan Arenado',
+  'Christian Yelich'
+);
+
+let question13 = new Question(
+  'Who was the NL Cy Young?',
+  'Mike Trout',
+  'Jacob DeGrom',
+  'Nolan Arenado',
+  'Christian Yelich'
+);
+
+let questionArray = [
+  question1,
+  question2,
+  question3,
+  question4,
+  question5,
+  question6,
+  question7,
+  question8,
+  question9,
+  question10,
+  question11,
+  question12,
+  question13
+];
+
+let questionTracker = [];
+
+// event listeners
+
+startGameButton.addEventListener('click', generateQuestion);
+correctField.addEventListener('click', playerHit);
+wrongField1.addEventListener('click', playerOut);
+wrongField2.addEventListener('click', playerOut);
+wrongField3.addEventListener('click', playerOut);
+restartButton.addEventListener('click', restartGame);
+nextButton.addEventListener('click', resetOverlay);
+startGameButton.addEventListener('click', startGame);
+restartWin.addEventListener('click', restartGame);
+closeWindowButton.addEventListener('click', closeWindow);
+restartLoss.addEventListener('click', restartGame);
+restartButton.addEventListener('click', restartGame);
+directionsButton.addEventListener('click', directionsMenuToggle);
+muteButton.addEventListener('click', muteMusic);
+
+// creates a random ordered question
+
+function generateQuestion() {
+  let randomQuestionNumber = Math.floor(Math.random() * questionArray.length);
+  let currentQuestionNumber = randomQuestionNumber;
+  questionField.innerHTML = questionArray[currentQuestionNumber].question;
+  correctField.innerHTML = questionArray[currentQuestionNumber].correct;
+  wrongField1.innerHTML = questionArray[currentQuestionNumber].wrong1;
+  wrongField2.innerHTML = questionArray[currentQuestionNumber].wrong2;
+  wrongField3.innerHTML = questionArray[currentQuestionNumber].wrong3;
+  questionTracker.push(questionArray[currentQuestionNumber]);
+  questionArray.splice(currentQuestionNumber, 1);
+  console.log(questionArray);
+  console.log(questionTracker);
+  // below from https://stackoverflow.com/questions/7070054/javascript-shuffle-html-list-element-order
+  for (var i = multiple.children.length; i >= 0; i--) {
+    multiple.appendChild(multiple.children[(Math.random() * i) | 0]);
+  }
+}
+
+// resets original question array on restart
+
+function resetQuestions() {
+  for (var i = 0; i < questionTracker.length; i++) {
+    questionArray.push(questionTracker[i]);
+    questionTracker.splice(i, 1);
+    i--;
+  }
+  console.log(questionArray);
+  console.log(questionTracker);
+}
 
 // preserve high score across sessions & clear score button
 
@@ -55,19 +243,6 @@ let fieldOptions = [
   'images/baseball2.png',
   'images/baseball3.png'
 ];
-
-//add event listeners to every question
-for (let i = 0; i < winner.length - 1; i++) {
-  winner[i].addEventListener('click', playerHit);
-}
-
-for (let i = 0; i < losers.length; i++) {
-  losers[i].addEventListener('click', playerOut);
-}
-
-nextButton.addEventListener('click', resetOverlay);
-startGameButton.addEventListener('click', startGame);
-startGameButton.addEventListener('click', startClock);
 
 // pitch clock timer (20 second time limit on questions) (helped by https://stackoverflow.com/questions/44314897/javascript-timer-for-a-quiz)
 
@@ -100,7 +275,7 @@ function timeOut() {
     pitchClock.innerHTML = ':0' + currentTimeInt;
   }
   if (currentTimeInt == 0 && currentOuts < 2) {
-    pitchClock.innerHTML = 'Out';
+    pitchClock.innerHTML = 'Out!';
     stopClock();
     playerOut();
   } else if (currentTimeInt == 0 && currentOuts >= 2) {
@@ -121,7 +296,7 @@ function playerHit() {
     }
     currentHits + 1;
     hits.innerHTML = 'Hits: ' + (currentHits += 1);
-    nextOverlay.style.zIndex = 100;
+    nextOverlay.style.zIndex = 3;
     hitSound.play();
     hitSound.volume = 0.2;
     pitchClock.innerHTML = 'Run!';
@@ -132,12 +307,21 @@ function playerHit() {
     currentRunners += 1;
     fieldNumber += 1;
     field.setAttribute('src', fieldOptions[fieldNumber]);
-    nextOverlay.style.zIndex = 100;
+    nextOverlay.style.zIndex = 3;
     hitSound.play();
     hitSound.volume = 0.2;
     pitchClock.innerHTML = 'Hit!';
     stopClock();
   }
+}
+
+// if user scores 10 runs, display win screen and restart option
+
+function tenRuns() {
+  winScreen.style.display = 'block';
+  currentRuns + 1;
+  highScore = 10;
+  localStorage.setItem('highScore', highScore);
 }
 
 //if answer is incorrect
@@ -147,35 +331,24 @@ function playerOut(evt) {
     outs.innerHTML = 'Outs: ' + currentOuts;
     booSound.play();
     booSound.volume = 0.2;
-    evt.target.style.color = 'red';
-    evt.target.style.textDecoration = 'line-through';
+    // evt.target.style.color = 'red';
+    // evt.target.style.textDecoration = 'line-through';
     pitchClock.innerHTML = 'Out!';
     stopClock();
   } else {
-    lossResult.style.zIndex = '100';
+    lossResult.style.zIndex = '3';
     outs.innerHTML = 'Outs: ' + 3;
     currentOuts = 0;
     booSound.play();
     booSound.volume = 0.2;
     pitchClock.innerHTML = 'Out!';
     stopClock();
-    for (let i = 0; i < triviaBox.length; i++) {
-      triviaBox[i].style.zIndex = 0;
-    }
-    questionNumber = 1;
   }
 }
 
 // restart game option or after button click
 
-restartLoss.addEventListener('click', restartGame);
-restartButton.addEventListener('click', restartGame);
-
 function restartGame() {
-  for (let i = 0; i < triviaBox.length; i++) {
-    triviaBox[i].style.zIndex = 0;
-  }
-  triviaBox[0].style.zIndex = zIndex + 3;
   lossResult.style.zIndex = '0';
   currentOuts = 0;
   currentRunners = 0;
@@ -191,11 +364,7 @@ function restartGame() {
   winScreen.style.display = 'none';
   nav.style.display = 'none';
   startGameOverlay.style.display = 'flex';
-  questionNumber = 1;
-  for (let i = 0; i < losers.length; i++) {
-    losers[i].style.color = 'white';
-    losers[i].style.textDecoration = 'none';
-  }
+  resetQuestions();
 }
 
 // bring up an overlay screen in between questions
@@ -203,10 +372,11 @@ function restartGame() {
 function resetOverlay() {
   if (currentRuns < 10) {
     nextOverlay.style.zIndex = 0;
-    triviaBox[questionNumber].style.zIndex = zIndex + 3;
-    questionNumber += 1;
+    multiple.style.color = 'white';
+    multiple.style.textDecoration = 'none';
     resetClock();
     startClock();
+    generateQuestion();
   }
 }
 
@@ -216,12 +386,11 @@ function startGame() {
   music.play();
   music.volume = 0.1;
   resetClock();
+  startClock();
   nav.style.display = 'block';
 }
 
 // directions menu overlay toggle
-
-directionsButton.addEventListener('click', directionsMenuToggle);
 
 function directionsMenuToggle() {
   if (directionsMenu.style.display === 'none') {
@@ -232,7 +401,6 @@ function directionsMenuToggle() {
 }
 
 // mute/play background music
-muteButton.addEventListener('click', muteMusic);
 
 function muteMusic() {
   if (
@@ -252,27 +420,7 @@ function muteMusic() {
   }
 }
 
-// add win game screen for question 13 (10 runs), with a restart option
-
-let x = winner.length - 1;
-
-winner[x].addEventListener('click', tenRuns);
-restartWin.addEventListener('click', restartGame);
-
-function tenRuns() {
-  winScreen.style.display = 'block';
-  for (let i = 0; i < triviaBox.length; i++) {
-    triviaBox[i].style.zIndex = 0;
-  }
-  currentRuns + 1;
-  highScore = 10;
-  localStorage.setItem('highScore', highScore);
-  questionNumber = 1;
-}
-
 // close directions menu button
-
-closeWindowButton.addEventListener('click', closeWindow);
 
 function closeWindow() {
   directionsMenu.style.display = 'none';
